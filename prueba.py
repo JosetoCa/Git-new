33c7c69dcd25f05543f7ca490ff08591515acb9f
JosetoCa <163927360+JosetoCa@users.noreply.github.com>
2025-02-20 11:30:14 -0500

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 11:15:00 2025
+
+@author: usuario
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, np.pi)
+
+y = np.sin(x)
+
+plt.plot(x,y)
+
+
+
